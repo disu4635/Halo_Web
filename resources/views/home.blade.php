31b7fdb80ee68bbf5176946dc56f9865c526a086
David Espinosa <davidespinosa4635@gmail.com>
2023-05-27 08:34:24 -0500

    Views de nobles añadidas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,13 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#181a1b] text-[#e8e6e3]">
-    <header class="flex m-5 items-center justify-center bg-zinc-900">
-        <h1 class="text-2xl font-bold">Halo Reach</h1>
-    </header>
     <div class="flex items-center justify-center h-screen mb-5">
         <img src="{{url('/assets/imagenes/home.png')}}" alt="Imagen" class="w-full h-full object-cover">
     </div>
-    <div class="flex m-10 items-center justify-center bg-zinc-900">
+    <header class="flex m-10 items-center justify-center bg-zinc-900">
         <h1 class="text-2xl font-semibold">Equipo Noble</h1>
-</div>
-    <div class="flex my-5">
+</header>
+    <section class="flex my-5">
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10">
                 <h1 class="my-4 text-center text-2xl font-medium font-medium">Carter Noble 1</h1>
@@ -24,12 +21,14 @@
             </div>
         </div>
         <div class="w-1/2 h-64 px-5">
+        <a href="{{url('noble1')}}" >
             <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/carter_home.jpeg')}}" alt="">
+        </a>
         </div>
-    </div>
-    <div class="flex my-5">
+    </section>
+    <section class="flex my-5">
         <div class="w-1/2 h-64 px-5">
-            <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/kat_home.jpg')}}" alt="">
+        <a href="{{url('noble2')}}"><img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/kat_home.jpg')}}" alt=""></a>
         </div>
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10" >
@@ -37,8 +36,8 @@
                 <p class="text-justify font-light">Kat es la especialista en tecnología del Equipo Noble, identificada como "Noble 2". Lleva una armadura púrpura y tiene un brazo cibernético.</p>
             </div>
         </div>
-    </div>
-    <div class="flex my-5">
+    </section>
+    <section class="flex my-5">
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10" >
                 <h1 class="my-4 text-center text-2xl font-medium">Jun Noble 3</h1>
@@ -46,12 +45,12 @@
             </div>
         </div>
         <div class="w-1/2 h-64 px-5">
-            <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/jun_home.jpg')}}" alt="">
+        <a href="{{url('noble3')}}"><img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/jun_home.jpg')}}" alt=""></a>
         </div>
-    </div>
-    <div class="flex my-5">
+    </section>
+    <section class="flex my-5">
         <div class="w-1/2 h-64 px-5">
-            <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/emile_home.jpg')}}" alt="">
+        <a href="{{url('noble4')}}"><img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/emile_home.jpg')}}" alt=""></a>
         </div>
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10" >
@@ -59,8 +58,8 @@
                 <p class="text-justify font-light">Emile es el especialista en combate cuerpo a cuerpo del Equipo Noble, conocido como "Noble 4". Lleva una armadura negra y un casco con una cuchilla en la visera.</p>
             </div>
         </div>
-    </div>
-    <div class="flex my-5">
+    </section>
+    <section class="flex my-5">
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10" >
                 <h1 class="my-4 text-center text-2xl font-medium">Jorge Noble 5</h1>
@@ -68,12 +67,12 @@
             </div>
         </div>
         <div class="w-1/2 h-64 px-5">
-            <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/jorge_home.jpg')}}" alt="">
+        <a href="{{url('noble5')}}"><img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/jorge_home.jpg')}}" alt=""></a>
         </div>
-    </div>
-    <div class="flex my-5">
+    </section>
+    <section class="flex my-5">
         <div class="w-1/2 h-64 px-5">
-            <img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/noble6_home.jpg')}}" alt="">
+        <a href="{{url('noble6')}}"><img class=" w-full h-full object-cover" src="{{url('/assets/imagenes/noble6_home.jpg')}}" alt=""></a>
         </div>
         <div class="w-1/2 flex items-center justify-center">
             <div class="mx-10" >
@@ -81,7 +80,7 @@
                 <p class="text-justify font-light">Noble Seis es un Spartan-III altamente habilidoso y valiente que desafía los obstáculos con determinación y tenacidad. Es un soldado eficiente y letal en el campo de batalla, capaz de adaptarse a diversas situaciones y enfrentar cualquier desafío que se le presente.</p>
             </div>
         </div>
-    </div>
+    </section>
 </body>
 <script>
     tailwind.config = {
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('test');
+// });
+
 Route::get('/', function () {
-    return view('test');
+    return view('home');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/noble1', function () {
+    return view('noble1');
+});
+
+Route::get('/noble2', function () {
+    return view('noble2');
+});
+
+Route::get('/noble3', function () {
+    return view('noble3');
+});
+
+Route::get('/noble4', function () {
+    return view('noble4');
+});
+
+Route::get('/noble5', function () {
+    return view('noble5');
+});
+
+Route::get('/noble6', function () {
+    return view('noble6');
 });
